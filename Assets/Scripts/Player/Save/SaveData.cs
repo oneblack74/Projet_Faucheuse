728769297288db501f8b4cdfce44c2902a338e3c
BRISSY Axel <axelbrissy0804@gmail.com>
2023-05-29 21:59:17 +0200

    Sauvegarde test 1

---
+++ Assets/Scripts/Player/Save/SaveData.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData : MonoBehaviour
+{
+    public Data data = new Data();
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Sauvegarder();
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Charger();
+        }
+    }
+
+    public void Sauvegarder()
+    {
+        string saveData = JsonUtility.ToJson(data);
+        string chemin = Application.persistentDataPath + "/SaveData.json";
+        Debug.Log(chemin);
+
+        System.IO.File.WriteAllText(chemin, saveData);
+    }
+
+    public void Charger()
+    {
+        string chemin = Application.persistentDataPath + "/SaveData.json";
+        string saveData = System.IO.File.ReadAllText(chemin);
+
+        data = JsonUtility.FromJson<Data>(saveData);
+    }
+}
+
+[System.Serializable]
+public class Data
+{
+    public int idStatue;
+}
